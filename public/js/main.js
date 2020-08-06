8bb0c9395b52283a99173941b65a6306553982fe
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2020-08-06 12:06:20 +0200

    Modularize frontend JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,140 +1,146 @@
 // should work without JS, progressive enhancement only
+import { ready, isFileTooLarge } from './lib/helpers.js';
+import { download } from './lib/download.js';
+import { postTranscribe } from './services/transcribe.js';
 
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
+class FormController {
+  constructor() {
+    this.el = new Map();
+    this.originalFileLabel = '';
   }
-}
 
-async function postForm(formData) {
-  const response = await fetch('/transcribe', {
-    method: 'POST',
-    body: formData,
-  });
+  init() {
+    const file = document.querySelector('#file');
+    const form = document.querySelector('#transcribe-form');
 
-  if (response.status >= 200 && response.status < 300) return response;
-  throw response;
-}
+    this.el.set('form', form);
+    this.el.set('submit', document.querySelector('#submit-btn'));
+    this.el.set('spinner', document.querySelector('.spinner'));
+    this.el.set('file', file);
+    this.el.set('fileLabel', document.querySelector('#file-label'));
+    this.el.set('output', document.querySelector('.form-output'));
+    this.el.set('outputError', document.querySelector('.form-output .error'));
 
-async function downloadResponse(response) {
-  const contentDisposition = response.headers.get('Content-Disposition');
-
-  const parsedFileName = /.*;filename\*=utf-8''(.*)$/.exec(contentDisposition);
-  const fileName = (parsedFileName && parsedFileName[1]) || 'transcript.txt';
-
-  const blob = await response.blob();
-  const url = window.URL.createObjectURL(blob);
-
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = decodeURIComponent(fileName);
-  a.style.display = 'none';
-  document.body.appendChild(a);
-  a.click();
-  window.URL.revokeObjectURL(url);
-}
-
-// this is checked on the server as well but might take a long time
-// Firefox throws a `NetworkError` when the BE connection is closed, re-using logic
-function checkFileSize(fileInputEl) {
-  const file = fileInputEl.files[0];
-  const maxSize = fileInputEl.dataset.limit;
-
-  const size = (file.size/1024/1024).toFixed(4); // MB
-
-  if (size > Number.parseInt(maxSize, 10)) {
-    throw new TypeError('NetworkError: File is too large');
+    file.addEventListener('change', this.onFileChange.bind(this));
+    form.addEventListener('submit', this.onFormSubmit.bind(this));
+    this.resetForm();
   }
-}
 
-ready(() => {
-  let originalText;
+  onFileChange(evt) {
+    this.setFormEnabled();
+    this.setFileLabel(evt.target.files[0].name);
+    this.resetOutput();
+  }
 
-  const el = {
-    form: document.querySelector('#transcribe-form'),
-    submit: document.querySelector('#submit-btn'),
-    spinner: document.querySelector('.spinner'),
-    file: document.querySelector('#file'),
-    fileLabel: document.querySelector('#file-label'),
-    output: document.querySelector('.form-output'),
-    outputError: document.querySelector('.form-output .error'),
-  };
-
-  if (!el.form) return;
-
-  const resetOutput = () => {
-    if (el.output.classList.contains('is-active')) {
-      el.outputError.innerHTML = '';
-      el.output.classList.remove('is-active');
-    }
-  };
-
-  const resetPostState = () => {
-    if (el.spinner.classList.contains('is-active')) {
-      el.spinner.classList.remove('is-active');
-    }
-
-    if (el.submit.hasAttribute('disabled')) {
-      el.submit.removeAttribute('disabled');
-    }
-  };
-
-  const resetForm = () => {
-    if (!el.submit.hasAttribute('disabled')) {
-      el.submit.setAttribute('disabled', '');
-    }
-
-    if (originalText) {
-      el.fileLabel.innerHTML = originalText;
-    }
-
-    el.form.reset();
-  };
-
-  // reset on refresh
-  resetForm();
-
-  el.file.addEventListener('change', (evt) => {
-    originalText = el.fileLabel.innerHTML;
-    el.fileLabel.innerHTML = evt.target.files[0].name;
-    el.submit.removeAttribute('disabled');
-    resetOutput();
-  });
-
-  el.form.addEventListener('submit', async (evt) => {
+  async onFormSubmit(evt) {
     evt.preventDefault();
-    resetOutput();
-    el.submit.setAttribute('disabled', '');
-    el.spinner.classList.add('is-active');
+
+    this.resetOutput();
+    this.showSpinner();
+    this.setFormDisabled();
 
     try {
-      checkFileSize(el.file);
-      const response = await postForm(new FormData(el.form));
+      const file = this.el.get('file');
+      if (isFileTooLarge(file.files[0], file.dataset.limit)) {
+        throw new TypeError('NetworkError: File too large');
+      }
+
+      const form = this.el.get('form');
+      const response = await postTranscribe(new FormData(form));
+
       // TODO: this might fail, add manual way to download
-      await downloadResponse(response);
-      resetPostState();
-      resetForm();
+      await download(response);
+
+      this.resetPostState();
+      this.resetForm();
     } catch (err) {
+      let errMessage = 'Internal Server Error';
+
       if (err.message && err.message.includes('NetworkError')) {
-        el.outputError.innerHTML = `Error: File is too large`;
+        errMessage = 'File is too large';
       } else if (err.json) {
         try {
           const body = await err.json();
-          el.outputError.innerHTML = `Error: ${body.message}`;
-        } catch (err) {
-          el.outputError.innerHTML = `Error: Internal Server Error`;
-        }
-      } else {
-        el.outputError.innerHTML = `Error: ${err.message}`;
+          errMessage = body.message;
+        } catch (err) {/**/}
+      } else if (err.message) {
+        errMessage = err.message;
       }
 
-      el.output.classList.add('is-active');
+      this.showError(errMessage);
     } finally {
-      resetPostState();
+      this.resetPostState();
+    }
+  }
+
+  resetForm() {
+    this.setFormDisabled();
+
+    if (this.originalFileLabel) {
+      this.setFileLabel(this.originalFileLabel)
     }
 
-    return false;
-  });
+    const form = this.el.get('form');
+    form.reset();
+  }
+
+  resetOutput() {
+    const output = this.el.get('output');
+    const outputError = this.el.get('outputError');
+
+    if (output.classList.contains('is-active')) {
+      outputError.innerHTML = '';
+      output.classList.remove('is-active');
+    }
+  }
+
+  resetPostState() {
+    const spinner = this.el.get('spinner');
+    const submit = this.el.get('submit');
+
+    if (spinner.classList.contains('is-active')) {
+      spinner.classList.remove('is-active');
+    }
+
+    if (submit.hasAttribute('disabled')) {
+      submit.removeAttribute('disabled');
+    }
+  }
+
+  setFormDisabled() {
+    const submit = this.el.get('submit');
+    submit.setAttribute('disabled', '');
+  }
+
+  setFormEnabled() {
+    const submit = this.el.get('submit');
+    submit.removeAttribute('disabled');
+  }
+
+  setFileLabel(name) {
+    const fileLabel = this.el.get('fileLabel');
+
+    this.originalFileLabel = fileLabel.innerHTML;
+    fileLabel.innerHTML = name;
+
+  }
+
+  showSpinner() {
+    const spinner = this.el.get('spinner');
+    spinner.classList.add('is-active');
+  }
+
+  showError(message) {
+    const output = this.el.get('output');
+    const outputError = this.el.get('outputError');
+
+    outputError.innerHTML = `Error: ${message}`;
+    output.classList.add('is-active');
+  }
+}
+
+const form = new FormController();
+
+ready(() => {
+  form.init();
 });
---
+++ public/js/services/transcribe.js
@@ -0,0 +1,10 @@
+
+export async function postTranscribe(formData) {
+  const response = await fetch('/transcribe', {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (response.status >= 200 && response.status < 300) return response;
+  throw response;
+}
